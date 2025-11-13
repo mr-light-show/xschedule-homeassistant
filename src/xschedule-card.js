159276af29bc63601c5433dece683eaeddd9e905
Keith Hawes <khawes@netflix.com>
2025-11-13 15:29:51 -0800

    chore: Bump version to 1.5.1-pre5

--- src/xschedule-card.js
+++ src/xschedule-card.js
@@ -1399,7 +1399,7 @@ customElements.define('xschedule-card', XScheduleCard);
 
 // Log card info to console
 console.info(
-  '%c  XSCHEDULE-CARD  \n%c  Version 1.5.1-pre4  ',
+  '%c  XSCHEDULE-CARD  \n%c  Version 1.5.1-pre5  ',
   'color: orange; font-weight: bold; background: black',
   'color: white; font-weight: bold; background: dimgray'
 );
--- src/xschedule-playlist-browser.js
+++ src/xschedule-playlist-browser.js
@@ -1045,7 +1045,7 @@ customElements.define('xschedule-playlist-browser', XSchedulePlaylistBrowser);
 
 // Log card info to console
 console.info(
-  '%c  XSCHEDULE-PLAYLIST-BROWSER  \n%c  Version 1.5.1-pre4  ',
+  '%c  XSCHEDULE-PLAYLIST-BROWSER  \n%c  Version 1.5.1-pre5  ',
   'color: orange; font-weight: bold; background: black',
   'color: white; font-weight: bold; background: dimgray'
 );
