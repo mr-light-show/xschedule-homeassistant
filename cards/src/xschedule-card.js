a98f7667b46a856580c5b5852386843d533a4cd5
Keith Hawes <khawes@netflix.com>
2025-11-20 17:42:47 -0800

    Release v1.7.4
    UI Improvement:
    - Make play/pause button same size as other playback controls (34px/24px)
    
    This change provides a more consistent visual appearance across all
    playback control buttons.

--- cards/src/xschedule-card.js
+++ cards/src/xschedule-card.js
@@ -1860,7 +1860,7 @@ customElements.define('xschedule-card', XScheduleCard);
 
 // Log card info to console
 console.info(
-  '%c  XSCHEDULE-CARD  \n%c  Version 1.7.3  ',
+  '%c  XSCHEDULE-CARD  \n%c  Version 1.7.4  ',
   'color: orange; font-weight: bold; background: black',
   'color: white; font-weight: bold; background: dimgray'
 );
